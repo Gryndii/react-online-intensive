275630a934ec2800dbfa31fdab046de204baec68
Gryndii <r.gryndii@gmail.com>
2019-06-11 19:02:12 +0300

    #4 Homework - Lesson4 Context

--- source/components/Composer/index.js
+++ source/components/Composer/index.js
@@ -1,20 +1,26 @@
 //Core
 import React, {Component} from 'react';
 
+//Components
+import {Consumer} from 'components/HOC/withProfile';
+
 //Instruments
 import Styles from './styles.m.css';
 
 class Composer extends Component{
     render() {
-        const {avatar, currentUserFirstName} = this.props;
         return(
-            <section className={Styles.composer}>
-                <img src={avatar} alt=""/>
-                <form>
-                    <textarea name="" id="" cols="30" rows="10" placeholder={`Whats on your mind, ${currentUserFirstName}?`}></textarea>
-                    <input type="submit" value="Post"/>
-                </form>
-            </section>
+            <Consumer>
+                {(context) => (
+                    <section className={Styles.composer}>
+                        <img src={context.avatar} alt=""/>
+                        <form>
+                            <textarea name="" id="" cols="30" rows="10" placeholder={`Whats on your mind, ${context.currentUserFirstName}?`}></textarea>
+                            <input type="submit" value="Post"/>
+                        </form>
+                    </section>
+                )}
+            </Consumer>
         );
     }
 }
--- source/components/Post/index.js
+++ source/components/Post/index.js
@@ -1,23 +1,29 @@
 //Core
 import React, {Component} from 'react';
 
+//Components
+import {Consumer} from 'components/HOC/withProfile';
+
 //Instruments
 import moment from 'moment';
 import Styles from './styles.m.css';
 
 class Post extends Component{
     render() {
-        const {avatar, currentUserFirstName, currentUserLastName} = this.props;
         return(
-            <section className={Styles.post}>
-                <img src={avatar} alt=""/>
-                <a>
-                    {currentUserFirstName}
-                    {currentUserLastName}
-                </a>
-                <time>{moment().format('MMMM D h:mm:ss a')}</time>
-                <p>Howdy!</p>
-            </section>
+            <Consumer>
+                {(context) => (
+                    <section className={Styles.post}>
+                        <img src={context.avatar} alt=""/>
+                        <a>
+                            {context.currentUserFirstName}
+                            {context.currentUserLastName}
+                        </a>
+                        <time>{moment().format('MMMM D h:mm:ss a')}</time>
+                        <p>Howdy!</p>
+                    </section>
+                )}
+            </Consumer>
         );
     }
 }
